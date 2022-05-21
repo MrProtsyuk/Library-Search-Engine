83a0ad0f769d925e27443e0cbe9a3501683419bc
Your Name <markprotsyuk@Marks-MacBook-Pro-2.local>
2022-05-20 18:06:15 -0700

    added query resolver

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -4,7 +4,17 @@ const { AuthenticationError } = require('apollo-server-express');
 
 //create resolvers
 const resolvers = {
-    
+    Query: {
+        me: async (parent, args, context) => {
+            if (context.user) {
+                const userData = await User.findOne({ _id: context.user._id })
+                .select('__v - password');
+
+                return userData;
+            }
+            throw new AuthenticationError('Not logged in');
+        }
+    }
 }
 
 //export file
