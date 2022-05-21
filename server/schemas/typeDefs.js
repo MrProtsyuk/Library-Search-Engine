fd02e482c71942f3c529171fadda2bd0cf250e71
Your Name <markprotsyuk@Marks-MacBook-Pro-2.local>
2022-05-20 17:52:10 -0700

    added book, query, and mutation typedefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -11,15 +11,23 @@ const typeDefs = gql`
     }
 
     type Book {
-
+        bookId: ID
+        authors: String
+        description: String
+        title: String
+        image: String
+        link: String
     }
 
     type Query {
-
+        me: User
     }
 
     type Mutation {
-
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(authors: String!, description: String!, title: String!, image: String!, link: String!): User
+        removeBook(bookId: ID!): User
     }
 
     type Auth {
